dd2408e4354a7d0addf0f1c41c7724091feb7744
Ed Britton <git@edbritton.com>
2026-05-16 12:46:42 +1000

    macOS: using yabai to quit applications when all windows are closed
    macOS: using yabai to quit applications when all windows are closed

---
+++ macOS/.config/yabai/autoquit.jsx
@@ -0,0 +1,31 @@
+#!/usr/bin/osascript -l JavaScript
+var app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+delay(0.2);
+
+var windowPIDs = new Set(
+  JSON.parse(app.doShellScript("yabai -m query --windows"))
+    .filter(function(w) { return w.title && w.title.length > 0; })
+    .map(function(w) { return w.pid; })
+);
+
+console.log("Window PIDs:", [...windowPIDs].join(", "));
+
+var SE = Application("System Events");
+var procs = SE.processes.whose({backgroundOnly: false});
+var exclude = new Set(["Finder","Dock","SystemUIServer","ControlCenter","Signal","Spotlight","Messages","Mail"]);
+for (var i = 0; i < procs.length; i++) {
+  var p = procs[i];
+
+  console.log("Checking:", p.name(), "PID:", p.unixId(), "Has window?", windowPIDs.has(p.unixId()));
+
+  if (!windowPIDs.has(p.unixId()) && !exclude.has(p.name())) {
+    console.log("Quitting:", p.name(), p.bundleIdentifier());
+
+    var bid = p.bundleIdentifier();
+    if (bid) try { Application(bid).quit(); } catch(e) {}
+
+    //app.doShellScript("/bin/kill -TERM " + p.unixId());
+  }
+}
--- macOS/.config/yabai/single_window.sh
+++ macOS/.config/yabai/single_window.sh
@@ -3,7 +3,7 @@
 EXCLUDED_APPS='^(LuLu|Calculator|Software Update|Dictionary|VLC|System Preferences|System Settings|zoom.us|Photo Booth|Archive Utility|Python|LibreOffice|App Store|Steam|Alfred|Activity Monitor)$'
 
 WINDOWS_IN_SPACE=$(yabai -m query --windows --space)
-WINDOW_COUNT=$(echo "$WINDOWS_IN_SPACE" | jq --arg excl "$EXCLUDED_APPS" '[.[] | select(."is-visible"==true and ."can-resize"==true and (.app | test($excl) | not) )] | length')
+WINDOW_COUNT=$(echo "$WINDOWS_IN_SPACE" | jq --arg excl "$EXCLUDED_APPS" '[.[] | select(."is-visible"==true and ."can-resize"==true and ."is-native-fullscreen"==false and (.app | test($excl) | not) )] | length')
 
 if [ "$WINDOW_COUNT" -eq 1 ]; then
   ID=$(echo "$WINDOWS_IN_SPACE" | jq -r --arg excl "$EXCLUDED_APPS" '[.[] | select(."is-visible"==true and ."is-floating"==false and ."can-resize"==true and ."is-native-fullscreen"==false and (.app | test($excl) | not) )][0].id')
