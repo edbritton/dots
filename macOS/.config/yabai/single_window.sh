d360916d02737a7fa11d4d404c03513007f48fc0
Ed Britton <git@edbritton.com>
2026-05-21 21:37:32 +1000

    macOS: use yabai for square-centre window

--- macOS/.config/yabai/single_window.sh
+++ macOS/.config/yabai/single_window.sh
@@ -2,7 +2,7 @@
 
 EXCLUDED_APPS='^(LuLu|Calculator|Software Update|Dictionary|VLC|System Preferences|System Settings|zoom.us|Photo Booth|Archive Utility|Python|LibreOffice|App Store|Steam|Alfred|Activity Monitor|Tips|FaceTime)$'
 
-sleep 0.2
+sleep 0.3
 
 WINDOWS_IN_SPACE=$(yabai -m query --windows --space)
 WINDOW_COUNT=$(echo "$WINDOWS_IN_SPACE" | jq --arg excl "$EXCLUDED_APPS" '[.[] | select(."is-visible"==true and ."can-resize"==true and ."is-native-fullscreen"==false and (.app | test($excl) | not) )] | length')
@@ -22,7 +22,8 @@ if [ "$WINDOW_COUNT" -eq 1 ]; then
     APP_NAME=$(echo "$WINDOWS_IN_SPACE" | jq --arg excl "$EXCLUDED_APPS" '[.[] | select(."is-visible"==true and ."can-resize"==true and ."is-native-fullscreen"==false and (.app | test($excl) | not) )][0].app')
 
     if [ "$APP_NAME" == \"Obsidian\" ]; then
-      skhd -k 'fn + ctrl - f'
+      #skhd -k 'fn + ctrl - f'
+      yabai -m window "$ID" --grid 1:1:0:0:1:1
     else
       #shortcuts run 'Square the window'
       res=$(system_profiler SPDisplaysDataType | grep Resolution | jq -R 'capture("Resolution:\\s*(?<w>[0-9]+)\\s*x\\s*(?<h>[0-9]+)") | [.w, .h] | map(tonumber)')
@@ -33,7 +34,7 @@ if [ "$WINDOW_COUNT" -eq 1 ]; then
     fi
   fi
 elif [ "$WINDOW_COUNT" -gt 1 ]; then
-  echo "$WINDOWS_IN_SPACE" | jq -r --arg excl "$EXCLUDED_APPS" '.[] | select(."is-visible"==true and ."is-floating"==true and ."can-resize"==true and (.app | test($excl) | not) ) .id' | while read -r ID; do
+  echo "$WINDOWS_IN_SPACE" | jq -r --arg excl "$EXCLUDED_APPS" 'reverse | .[] | select(."is-visible" == true and ."is-floating" == true and ."can-resize" == true and (.app | test($excl) | not) ) .id' | while read -r ID; do
     yabai -m window $ID --toggle float &&
       yabai -m window $ID --swap west
   done
